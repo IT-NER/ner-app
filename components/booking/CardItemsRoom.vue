ea876c2da8abb9d3b50fef12e90c5896d9ba5cb8
kaow-dev-2024 <kaow.dev.2024@gmail.com>
2024-06-24 14:14:11 +0700

    main

--- components/booking/CardItemsRoom.vue
+++ components/booking/CardItemsRoom.vue
@@ -2,17 +2,23 @@
   <div>
     <v-container>
       <v-row>
-        <v-col cols="12">
-          <v-chip
-            v-for="(item, i) in Room"
-            :key="i"
-            label
-            :color="item.color"
-            dark
-            class="ma-2"
-          >
-            {{ item.name }}
-          </v-chip>
+        <v-col>
+          <v-card outlined>
+            <v-card-actions>
+              <v-col cols="12">
+                <v-chip
+                  v-for="(item, i) in Room"
+                  :key="i"
+                  label
+                  :color="item.color"
+                  dark
+                  class="ma-2"
+                >
+                  {{ item.name }}
+                </v-chip>
+              </v-col>
+            </v-card-actions>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
